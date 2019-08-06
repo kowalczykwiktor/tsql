c0cb8d317f2b46110d4ca9cb64b20194ced62a94
Wiktor Kowalczyk <kowalczyk.wiktor@gmail.com>
2019-08-06 22:31:08 +0200

    checkingExistingViews

--- ifExistForViews.sql
+++ ifExistForViews.sql
@@ -1,1 +1,3 @@
-IF EXISTS(SELECT *          FROM sys.objects          WHERE object_id = OBJECT_ID(N'[dbo].[BIAListView]') AND type IN (N'V'))    DROP VIEW [dbo].[BIAListView]
+IF EXISTS(SELECT *
+          FROM sys.objects
+          WHERE object_id = OBJECT_ID(N'[dbo].[BIAListView]') AND type IN (N'V'))
